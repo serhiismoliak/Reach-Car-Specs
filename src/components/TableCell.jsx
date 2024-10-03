864f345a33ac49a0a6d3186b002a24fa88af7cea
Serhii Smoliak <serhiysmoliak@gmail.com>
2024-10-03 22:29:52 +0300

    fixed bug in table cell

--- src/components/TableCell.jsx
+++ src/components/TableCell.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const TableCell = (specs) => {
-    function capitalize(str='') {str[0].toUpperCase() + str.slice(1);}
+const TableCell = ({specs}) => {
+    function capitalize(str='') {return str[0].toUpperCase() + str.slice(1);}
 
-    
     return (
         <td>
             <ul>
                 {Object.keys(specs).filter(tag => tag !== 'id')
-                    .map((tag, index) => <li key={index}>{capitalize(tag)}: {specs[tag]}</li>
-                )}
+                    .map((tag, index) => <li key={index}>{capitalize(tag)}: {specs[tag]}</li>)
+                    
+                }
             </ul>
         </td>
     );
